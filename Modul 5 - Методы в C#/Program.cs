ff47f979771e8da22a849e5418baa992e77655bf
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-24 10:17:37 +0200

    Добавьте файлы проекта.

---
+++ Modul 5 - Методы в C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+class Programm
+{
+  public static void Main(string[] args)
+    {
+       ( string name, string[] dishes) user;
+        Console.Write("Ваше имя - ");
+        user.name = Console.ReadLine();
+
+        user.dishes = new string[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Ваше {0} любимое блюдо - ", i + 1);
+            user.dishes[i] = Console.ReadLine();
+
+        }
+        Console.ReadKey();
+
+    }
+}
